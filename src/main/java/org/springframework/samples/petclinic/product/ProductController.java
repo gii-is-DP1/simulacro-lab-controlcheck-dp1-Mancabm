bc09b6c4449eb908c0bc4317cac4d3d5f0283c28
Mancbm <silverlancernohr@gmail.com>
2021-12-21 12:30:43 +0100

    Control Check

--- src/main/java/org/springframework/samples/petclinic/product/ProductController.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductController.java
@@ -1,5 +1,24 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class ProductController {
-    
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/product/create")
+    public String createOrUpdateProduct(@Valid Product product, BindingResult result){
+        if(result.hasErrors()){
+            return "products/createOrUpdateProductForm";
+        } else{
+            productService.save(product);
+            return "welcome";
+        }
+    }
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,14 +3,21 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 
-
-public interface ProductRepository {
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Integer>{
     List<Product> findAll();
+    @Query("SELECT producttype FROM ProductType producttype")
     List<ProductType> findAllProductTypes();
     Optional<Product> findById(int id);
     Product findByName(String name);
     Product save(Product p);
+    @Query("SELECT producttype FROM ProductType producttype WHERE producttype.name =:name")
+    ProductType getProductTypeByName(String name);
+    @Query("SELECT product FROM Product product WHERE product.price <:precio")
+    List<Product> findByPriceLessThan(Double precio);
 }
